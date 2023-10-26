5434d4dffc48c1977f5aace704b0d7011502da4b
yhwu66 <97576976+yhwu66@users.noreply.github.com>
2023-10-26 16:35:53 -0700

    add 4 tasks

--- Code/DeliveryChallenge/Assets/Script/Task/TaskInfo.cs
+++ Code/DeliveryChallenge/Assets/Script/Task/TaskInfo.cs
@@ -52,7 +52,7 @@ public class TaskData
 {
     public string[] taskTitles =
     {
-        "Pizza", "Fried Chicken","Fruits"
+        "Pizza", "Fried Chicken","Fruits","French Fires","Sushi","Hamburger","Popcorn"
     };
     
     public string[] taskGetName =
@@ -60,6 +60,10 @@ public class TaskData
         "Pizza Store",
         "Fried Chicken Store",
         "Fruits Store",
+        "New Fast Food",
+        "Restaurant",
+        "Bar",
+        "Supermarket"
     };
     
     public string[] taskDestination =
@@ -67,6 +71,11 @@ public class TaskData
         "Auto Service",
         "Music Store",
         "Clothing Store",
+        "Green Apartment",
+        "Green House",
+        "Red House",
+        "Yellow Apartment",
+        
     };
     Dictionary<string, int> taskTitlesIndex = new Dictionary<string, int>();
     Dictionary<string, int> taskDestinationIndex = new Dictionary<string, int>();
@@ -81,7 +90,7 @@ public class TaskData
         Color.yellow,
         Color.cyan,
         Color.magenta,
-        Color.white,
+        new Color(0.5f, 0.25f, 0.75f),
         Color.grey,
         Color.black,
         new Color(0.5f, 0.25f, 0.75f)  
@@ -89,13 +98,13 @@ public class TaskData
 
     public Vector3[] taskGetPositions =
     {
-        new Vector3(0.5199966f, -3.69f, 7.28f), //Pizza
-        new Vector3(-15.43f, -3.69f, -36.27f), //Fried Chicken
-        new Vector3(-79.90f, -3.79f, -94.60f), // Fruits
-        new Vector3(0.5199966f, -3.69f, 7.28f),
-        new Vector3(0.5199966f, -3.69f, 7.28f),
-        new Vector3(0.5199966f, -3.69f, 7.28f),
-        new Vector3(0.5199966f, -3.69f, 7.28f),
+        new Vector3(0.5199966f, -3.69f, 7.28f), //Pizza Store
+        new Vector3(-15.43f, -3.69f, -36.27f), //Fried Chicken Store
+        new Vector3(-79.90f, -3.79f, -94.60f), //Fruits Store
+        new Vector3(-2.09f, -3.75f, -36.23f), //New Fast Food
+        new Vector3(-6.2f, -3.75f, -52.8f), //Restaurant
+        new Vector3(-41.48f, -3.75f, -52.8f), //Bar
+        new Vector3(-60.8f, -3.75f, -5.1f), // Supermarket
         new Vector3(0.5199966f, -3.69f, 7.28f),
         new Vector3(0.5199966f, -3.69f, 7.28f),
         new Vector3(0.5199966f, -3.69f, 7.28f),
@@ -103,13 +112,13 @@ public class TaskData
 
     public Quaternion[] taskGetRotations =
     {
-        Quaternion.Euler(0,0,0),//Pizza
-        Quaternion.Euler(0,0,0),//Fried Chicken
-        Quaternion.Euler(0,45,0),//Fruits
-        Quaternion.Euler(0,0,0),
-        Quaternion.Euler(0,0,0),
-        Quaternion.Euler(0,0,0),
-        Quaternion.Euler(0,0,0),
+        Quaternion.Euler(0,0,0),//Pizza Store
+        Quaternion.Euler(0,0,0),//Fried Chicken Store
+        Quaternion.Euler(0,45,0),//Fruits Store
+        Quaternion.Euler(0,0,0),//New Fast Food
+        Quaternion.Euler(0,0,0),//Restaurant
+        Quaternion.Euler(0,0,0),//Bar
+        Quaternion.Euler(0,0,0),// Supermarket
         Quaternion.Euler(0,0,0),
         Quaternion.Euler(0,0,0),
         Quaternion.Euler(0,0,0),
@@ -120,10 +129,10 @@ public class TaskData
         Quaternion.Euler(0,0,0), //Auto
         Quaternion.Euler(0,0,0), //Music
         Quaternion.Euler(0,45,0), //Clothing
-        Quaternion.Euler(0,0,0),
-        Quaternion.Euler(0,0,0),
-        Quaternion.Euler(0,0,0),
-        Quaternion.Euler(0,0,0),
+        Quaternion.Euler(0,90,0), //Green Apartment
+        Quaternion.Euler(0,90,0), //Green House
+        Quaternion.Euler(0,0,0), //Red House
+        Quaternion.Euler(0,0,0), //Yellow Apartment
         Quaternion.Euler(0,0,0),
         Quaternion.Euler(0,0,0),
         Quaternion.Euler(0,0,0),
@@ -136,10 +145,10 @@ public class TaskData
         new Vector3(-43.13f, -3.88f, -92.776f), //Auto Service
         new Vector3(-11.97f, -3.79f, -95.98f), //Music Store
         new Vector3( 20.0f, -3.79f, 5.98f), //clothing store
-        new Vector3(-43.13f, -3.88f, -92.776f),
-        new Vector3(-43.13f, -3.88f, -92.776f),
-        new Vector3(-43.13f, -3.88f, -92.776f),
-        new Vector3(-43.13f, -3.88f, -92.776f),
+        new Vector3(-82.24f, -3.75f, -62.81f),//Green Apartment
+        new Vector3(-38.4f, -3.75f, -71.7f), //Green House
+        new Vector3(-72.9f, -3.75f, -24.41f), //Red House
+        new Vector3(-60.8f, -3.75f, -52.2f), // Yellow Apartment
         new Vector3(-43.13f, -3.88f, -92.776f),
         new Vector3(-43.13f, -3.88f, -92.776f),
         new Vector3(-43.13f, -3.88f, -92.776f),
